90cc73cd217f93f94c98d48be7c8971d5cc2f139
Shaurya Fulfagar <80444101+ShauryaFulfagar@users.noreply.github.com>
2024-08-23 19:38:55 +0530

    Switched to Flask for backend

--- face_recg/recognition/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    embedding = models.BinaryField()
-
-    def __str__(self):
-        return self.name
--- face_recg/recognition/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('sign-up/', views.sign_up),
-    path('sign-in/', views.sign_in),
-]
