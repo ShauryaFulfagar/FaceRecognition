88ff4eecaac59960617ff742c265af316788d487
Shaurya Fulfagar <80444101+ShauryaFulfagar@users.noreply.github.com>
2024-08-23 20:15:35 +0530

    Camera instead of image

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://recg_user:recg_pass@localhost/face_recg_db'
-db = SQLAlchemy(app)
-
-from app import routes
--- app/models.py
+++
@@ -1,6 +0,0 @@
-from app import db
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100), nullable=False)
-    encoding = db.Column(db.LargeBinary, nullable=False)
